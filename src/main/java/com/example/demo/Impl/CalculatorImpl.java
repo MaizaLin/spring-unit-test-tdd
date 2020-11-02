71db4a3cfe31078e7f8bdff31d786a882d18c6f0
门店研发-林云琦 ( linyunqi ) <linyunqi@tuhu.cn>
2020-11-02 18:03:10 +0800

    需求1 加法计算器 红灯

---
+++ src/main/java/com/example/demo/Impl/CalculatorImpl.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2020 tuhu.cn All right reserved. This software is the
+ * confidential and proprietary information of tuhu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tuhu.cn
+ */
+package com.example.demo.Impl;
+
+
+import com.example.demo.Calculator;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Linyunqi
+ * @date 2020/11/214:13
+ */
+
+@Component("CalculatorImpl")
+public class CalculatorImpl implements Calculator {
+
+    @Override
+    public Double execute(Double num1, Double num2) {
+        return 0D;
+    }
+}
---
+++ src/test/java/com/example/demo/Impl/CalculatorImplTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 tuhu.cn All right reserved. This software is the
+ * confidential and proprietary information of tuhu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tuhu.cn
+ */
+package com.example.demo.Impl;
+
+import com.example.demo.Calculator;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Linyunqi
+ * @date 2020/11/214:30
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+public class CalculatorImplTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Autowired
+    @Qualifier("CalculatorImpl")
+    Calculator calculator;
+
+    @Test
+    public void TestAdd() {
+        Assert.assertTrue("简单加法测试", calculator.execute(1D, 1D).equals(2D));
+    }
+
+
+}
