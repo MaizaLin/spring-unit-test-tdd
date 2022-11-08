661c407c5221eb1f2c946fb38be75b85180ce799
linyunqi <linyunqi2@tuhu.cn>
2022-11-08 19:55:01 +0800

    change domain

--- src/main/java/com/example/demo/OperatorEnum.java
+++ src/main/java/com/example/demo/OperatorEnum.java
@@ -1,9 +1,9 @@
 /*
- * Copyright 2020 tuhu.cn All right reserved. This software is the
- * confidential and proprietary information of tuhu.cn ("Confidential
+ * Copyright 2020 aaa.cn All right reserved. This software is the
+ * confidential and proprietary information of aaa.cn ("Confidential
  * Information"). You shall not disclose such Confidential Information and shall
  * use it only in accordance with the terms of the license agreement you entered
- * into with Tuhu.cn
+ * into with aaa.cn
  */
 package com.example.demo;
 
